023cf2fd3c2c48437646cf3bf1af775895c0ae4b
Vaniaina <hei.vaniaina@gmail.com>
2025-07-31 07:34:11 +0300

    initial commit

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-from urllib import request
-
 from fastapi import FastAPI
-from starlette.responses import Response
 
 app = FastAPI()
 
@@ -11,8 +8,6 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/hello")
-def hello_page():
-    with open("Hello.html","r",encoding="utf-8") as file:
-        html_content = file.read()
-    return Response(content=html_content, status_code=200, media_type="text/html")
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
