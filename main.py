9553a374142de16535c5da8ea66776bafc4d50f2
Vaniaina <hei.vaniaina@gmail.com>
2025-07-31 08:30:51 +0300

    ADD: question 3

--- main.py
+++ main.py
@@ -18,7 +18,13 @@ def pong():
 
 @app.get("/home")
 def home():
-    with open("welcome.html", "r",encoding="utf-8") as file:
-        html_content = file.read()
+    with open("welcome.html", "r",encoding="utf-8") as welcome:
+        html_content = welcome.read()
     return Response(content=html_content, status_code=200 ,media_type="text/html")
+
+@app.get("/{invalid_path}")
+def invalid_path(invalid_path:str):
+    with open("404.html", "r",encoding="utf-8") as not_found:
+        html_content = not_found.read()
+    return Response(content=html_content, status_code=404 ,media_type="text/html")
 
